4a47e5f67f15879557be774ae239e6753b875d3d
Gabrielmz21 <gabanmur@espol.edu.ec>
2019-01-28 00:06:25 -0500

    Anadimos proyecto Android Studio

---
+++ PST_CARRO/app/src/main/java/com/example/pst_carro/carga.java
@@ -0,0 +1,162 @@
+package com.example.pst_carro;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.JsonReader;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+
+public class carga extends AppCompatActivity {
+
+    ArrayAdapter mArrayAdapter;
+    ListView listView;
+    TextView textViewPeso, textViewLista;
+    Button buttonReiniciarLista, buttonCargar;
+    double pesoLimite = 0;
+    double pesoCarga = 0;
+    String id_usuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga);
+
+        listView = (ListView) findViewById(R.id.listView);
+        textViewPeso = (TextView) findViewById(R.id.textViewPeso);
+        textViewLista = (TextView) findViewById(R.id.textViewLista);
+        buttonCargar = (Button) findViewById(R.id.buttonCargar);
+        buttonCargar.setEnabled(true);
+        buttonReiniciarLista = (Button) findViewById(R.id.buttonReiniciarLista);
+        mArrayAdapter = new ArrayAdapter(this,R.layout.textviewlayout);
+        listView.setAdapter(mArrayAdapter);
+
+        Bundle bundle = getIntent().getExtras();
+        id_usuario = bundle.getString("id_usuario");
+
+        new consultarPesoLimite().execute("http://172.18.32.88/consultar_pesoLimite.php?id_usuario="+id_usuario);
+
+        new consultarItems().execute("http://172.18.32.88/consultar_item.php?id_usuario="+id_usuario);
+
+        buttonCargar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(carga.this,bluetooth.class);
+                startActivity(i);
+            }
+        });
+
+
+    }
+
+
+    private class consultarItems extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            JSONArray ja = null;
+            String[] token;
+            String[] token2;
+            int length = result.length();
+            int i;
+            token=result.split("]");
+            try {
+                for (i=0;i<length;i++){
+                    token2=token[i].replace("[","").replace("\"","").split(",");
+                    ja = new JSONArray(token2);
+                    mArrayAdapter.add(ja.getString(0) + "\n" + ja.getString(1));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private class consultarPesoLimite extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        Log.i("URL",""+myurl);
+        myurl = myurl.replace(" ","%20");
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("respuesta", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+}
---
+++ PST_CARRO/app/src/main/java/com/example/pst_carro/login.java
@@ -0,0 +1,146 @@
+package com.example.pst_carro;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class login extends AppCompatActivity {
+
+    EditText editTextUser;
+    EditText editTextPassword;
+    Button buttonLogin;
+    String user = "defaultuser";
+    String password = "defaultpassword";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextUser = (EditText) findViewById(R.id.editTextUser);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new loginApp().execute("http://172.18.32.88/login.php?id_usuario="+editTextUser.getText().toString());
+
+
+                if (editTextUser.getText().toString().equals(user)){
+                    if (editTextPassword.getText().toString().equals(password)){
+                        Intent i = new Intent(login.this,carga.class);
+                        i.putExtra("id_usuario",user);
+                        startActivity(i);
+                        editTextUser.setText("");
+                        editTextPassword.setText("");
+                        user = "defaultuser";
+                        password = "defaultpassword";
+                        finish();
+
+                    }else{
+                        editTextUser.setText("");
+                        editTextPassword.setText("");
+                        Toast.makeText(login.this,"Credenciales no validas",Toast.LENGTH_SHORT).show();
+                    }
+                }else {
+                    editTextUser.setText("");
+                    editTextPassword.setText("");
+                    Toast.makeText(login.this,"Credenciales no validas",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+
+    private class loginApp extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+
+            JSONArray ja = null;
+            try {
+                ja = new JSONArray(result);
+                user = ja.getString(0);
+                password = ja.getString(1);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        Log.i("URL",""+myurl);
+        myurl = myurl.replace(" ","%20");
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("respuesta", "The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+
+}
